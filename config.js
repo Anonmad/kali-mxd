522843366c73222c538b705b479244dd8ab900e2
ANONMAD NETWORK <anonymousmad74@gmail.com>
2026-03-03 04:12:12 +0300

    Add files via upload

--- config.js
+++ config.js
@@ -4,6 +4,6 @@ module.exports = {
   OWNER: process.env.OWNER || "255619615065",
   AUTO_VIEW_STATUS: true,
   ANTI_DELETE: true,
-  OPENAI_KEY: process.env.OPENAI_KEY || "",
+  OPENAI_KEY: process.env.OPENAI_KEY || "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",
   BOT_NAME: "KALI MD"
 }
